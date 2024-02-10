490210cf42292363e8bca934f5b93928e65155ba
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2024-02-10 16:58:42 -0500

    Set up basic window context

--- imageProcessor/src/main/java/parallel/group/imageprocessor/App.java
+++ imageProcessor/src/main/java/parallel/group/imageprocessor/App.java
@@ -1,20 +1,25 @@
 package parallel.group.imageprocessor;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.Group;
 import java.io.File;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage = new Stage();
+        primaryStage.setTitle("Image Processor");
 
+        int width = 720;
+        int height = 720;
+        Group rootNode = new Group();
+        rootNode.
+        Scene scene = new Scene(rootNode ,width, height);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
