d8bc76531bde907420b39768a9e2fb35a84483b1
zeorphix <tieue45@gmail.com>
2024-03-03 14:37:45 -0500

    Create Contour.java

---
+++ imageProcessor/src/main/java/parallel/group/imageprocessor/Contour.java
@@ -0,0 +1,15 @@
+package parallel.group.imageprocessor;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contour {
+    
+}
