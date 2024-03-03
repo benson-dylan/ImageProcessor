62ac2d92117b3de0ac644c83f35f1ad2813d80a0
zeorphix <tieue45@gmail.com>
2024-03-03 14:40:10 -0500

    Update Contour.java

--- imageProcessor/src/main/java/parallel/group/imageprocessor/Contour.java
+++ imageProcessor/src/main/java/parallel/group/imageprocessor/Contour.java
@@ -11,5 +11,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Contour {
-    
+    private BufferedImage image;
+    private final int NUM_THREADS = 8;
+
+    public Contour (BufferedImage image)
+    {
+        this.image = image;
+    }
+
+    public BufferedImage contour()
+    {
+        System.out.println("Attempting to find countours...");
+
+        try
+        {
+
+        }
+
+        finally
+        {
+
+        }
+    }
 }
