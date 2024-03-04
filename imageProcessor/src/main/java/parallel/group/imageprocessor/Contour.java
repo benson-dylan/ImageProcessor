bbdca188df5ca5965cca8b7f2f2797ed8e23a1ed
zeorphix <tieue45@gmail.com>
2024-03-03 19:16:55 -0500

    more helper functions

--- imageProcessor/src/main/java/parallel/group/imageprocessor/Contour.java
+++ imageProcessor/src/main/java/parallel/group/imageprocessor/Contour.java
@@ -36,7 +36,11 @@ public class Contour {
 
         try
         {
-            
+            // convert BufferedImage to Mat
+            Mat src = new Mat(height, widdth, CvType.CV_8UC3);
+            bufferedImageToMat(image, src);
+
+            // convert to grayscale
         }
 
         catch (Exception error)
@@ -54,12 +58,33 @@ public class Contour {
     }
 }
 
+private List<MatOfPoint> findContours(Mat submat)
+{
+    
+}
+
 private Mat bufferedImageToMat(BufferedImage image)
 {
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    
+    ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
 
+    byteArrayOutputStream.flush();
+
+    byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+    byteArrayOutputStream.close();
+
+    mat.put(0, 0, byteArray);
 }
 
 private void matToBufferedImage(Mat mat, BufferedImage iamge)
 {
+    int width = mat.width(), height = mat.height(), channels = mat.channels();
 
+    byte[] sourcePixels = new byte[width * height * channels];
+
+    mat.get(0, 0, sourcePixels);
+
+    bufferedImage.getRaster().setDataElements(0, 0, width, height, sourcePixels);
 }
